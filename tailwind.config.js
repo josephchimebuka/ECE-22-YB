38c745754f40b2da707cf6dc73c189d8a4120a07
josephchimebuka <chimebujoseph@gmail.com>
2023-08-07 05:12:23 +0100

    changes added

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    fontFamily:{
+      times: ['Times New Roman', 'san-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
